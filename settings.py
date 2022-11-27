058005c48c0519d945f6b8b53fd444f9f6b447b5
istrybuk <79363807+istrybuk@users.noreply.github.com>
2022-11-27 22:54:01 +0300

    настройка Pycharm с GitHub

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-VALID_EMAIL = 'Sivan@bk.by'
-VALID_PASSWORD = '123Qaz'
-TEST_EMAIL = 'test@4.com'
-TEST_PASS = '12345Q'
-RT_EMAIL = 'test@4.com'
-RT_PASSWORD = '12345Q'
+VALID_EMAIL = 'Sivan@bk.by' # действующий
+VALID_PASSWORD = '123Qaz' # действующий
+TEST_EMAIL = 'test@4.com' # для  рег
+TEST_PASS = '12345Q' # для  рег
+RT_EMAIL = 'test@4.com' # для логина
+RT_PASSWORD = '12345Q' # Для логина
